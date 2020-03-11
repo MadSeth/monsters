2471d6125e2f1b2ebd3d46185b8512013bebee80
MadSeth <vmihai23@gmail.com>
2020-03-11 19:56:10 +0200

    Updates

---
+++ precache-manifest.02bd19dd4f59dea6075bc95b02b99143.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "335b884d9ef7f8a18f5f60a0352b2f7a",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "7c78c1e3818e71a8d622",
+    "url": "/monsters/static/css/main.e69a5bff.chunk.css"
+  },
+  {
+    "revision": "3dd738a58da8e2f40ee0",
+    "url": "/monsters/static/js/2.2fdbc417.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters/static/js/2.2fdbc417.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c78c1e3818e71a8d622",
+    "url": "/monsters/static/js/main.1208a11a.chunk.js"
+  },
+  {
+    "revision": "e103ed0a03ca5e0ce3a2",
+    "url": "/monsters/static/js/runtime-main.e35ab045.js"
+  }
+]);
